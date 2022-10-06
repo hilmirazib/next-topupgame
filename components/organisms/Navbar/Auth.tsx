852cea61d4639e0b1ecb90236428913e06030a57
hilmi.inolabs <hilmi.inolabs@gmail.com>
2022-10-06 23:40:36 +0700

    Navbar Condition and Logout

--- components/organisms/Navbar/Auth.tsx
+++ components/organisms/Navbar/Auth.tsx
@@ -1,16 +1,49 @@
 import Link from 'next/link';
-interface authLogin {
-  isLogin?: boolean;
-}
-export default function Auth(props: authLogin) {
-  const { isLogin } = props;
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import jwtDecode from 'jwt-decode';
+import { useRouter } from 'next/router';
+import { JWTPayloadTypes, UserTypes } from '../../../serviceAPI/data-type';
+// interface authLogin {
+//   isLogin?: boolean;
+// }
+// export default function Auth(props: authLogin) {
+export default function Auth() {
+  // const { isLogin } = props;
+  const [isLogin, setIsLogin] = useState(false);
+  const [user, setUser] = useState({
+    avatar: '',
+    email: '',
+    id: '',
+    name: '',
+    username: '',
+  });
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookies.get('token');
+    if (token) {
+      const jwtToken = atob(token);
+      const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+      const userPayload: UserTypes = payload.player;
+      const urlUpload = process.env.NEXT_PUBLIC_IMG;
+      userPayload.avatar = `${urlUpload}/${userPayload.avatar}`;
+      setIsLogin(true);
+      setUser(userPayload);
+    }
+  }, []);
+  const onLogout = () => {
+    Cookies.remove('token');
+    router.push('/');
+    setIsLogin(false);
+  };
   if (isLogin) {
     return (
       <li className="nav-item my-auto dropdown d-flex">
         <div className="vertical-line d-lg-block d-none"></div>
         <div>
           <a className="dropdown-toggle ms-lg-40" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="/img/avatar-1.png" className="rounded-circle" width="40" height="40" alt="" />
+            <img src={user.avatar} className="rounded-circle" width="40" height="40" alt="" />
           </a>
 
           <ul className="dropdown-menu border-0" aria-labelledby="dropdownMenuLink">
@@ -35,12 +68,10 @@ export default function Auth(props: authLogin) {
                 </a>
               </Link>
             </li>
-            <li>
-              <Link href="/sign-in">
-                <a className="dropdown-item text-lg color-palette-2" href="#">
-                  Log Out
-                </a>
-              </Link>
+            <li onClick={onLogout}>
+              <a className="dropdown-item text-lg color-palette-2" href="#">
+                Log Out
+              </a>
             </li>
           </ul>
         </div>

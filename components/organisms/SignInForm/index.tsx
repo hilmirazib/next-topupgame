f1941e88db89a11010b8b1b48e6f24400061855d
hilmi.inolabs <hilmi.inolabs@gmail.com>
2022-10-06 11:50:23 +0700

    integrasi signin

--- components/organisms/SignInForm/index.tsx
+++ components/organisms/SignInForm/index.tsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
 import Link from 'next/link';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+import cn from 'classnames';
+import { setSignIn } from '../../../serviceAPI/auth';
 export default function SignInForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const className = {
+    label: cn('form-label text-lg fw-medium color-palette-1 mb-10'),
+  };
+  const onSubmit = async () => {
+    const data = {
+      email,
+      password,
+    };
+    if (!email || !password) {
+      toast.error('Email dan Password wajib diisi!!!');
+    } else {
+      const response = await setSignIn(data);
+      if (response.error) {
+        toast.error(response.message);
+      } else {
+        toast.success('Login Berhasil');
+        const { token } = response.data;
+        router.push('/');
+      }
+    }
+  };
   return (
     <>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign In</h2>
       <p className="text-lg color-palette-1 m-0">Masuk untuk melakukan proses top up</p>
       <div className="pt-50">
-        <label htmlFor="email" className="form-label text-lg fw-medium color-palette-1 mb-10">
+        <label htmlFor="email" className={className.label}>
           Email Address
         </label>
         <input
@@ -15,10 +44,12 @@ export default function SignInForm() {
           name="email"
           aria-describedby="email"
           placeholder="Enter your email address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
       <div className="pt-30">
-        <label htmlFor="password" className="form-label text-lg fw-medium color-palette-1 mb-10">
+        <label htmlFor="password" className={className.label}>
           Password
         </label>
         <input
@@ -28,14 +59,14 @@ export default function SignInForm() {
           name="password"
           aria-describedby="password"
           placeholder="Your password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <Link href="/">
-          <a className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16" role="button">
-            Continue to Sign In
-          </a>
-        </Link>
+        <button type="button" className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16" onClick={onSubmit}>
+          Continue to Sign In
+        </button>
         <Link href="/sign-up">
           <a className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill" role="button">
             Sign Up
--- serviceAPI/auth.ts
+++ serviceAPI/auth.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { LoginTypes } from './data-type';
 
 const ROOT_API = process.env.NEXT_PUBLIC_API;
 const API_VERSION = 'api/v1';
@@ -12,6 +13,22 @@ export async function setSignUp(data: FormData) {
   }
   return axiosResponse.data;
 }
-export async function setSignIn() {
-  return null;
+export async function setSignIn(data: LoginTypes) {
+  const URL = 'auth/signin';
+  const response = await axios.post(`${ROOT_API}/${API_VERSION}/${URL}`, data).catch((err) => err.response);
+  console.log('res', response);
+  if (response.status > 300) {
+    const res = {
+      error: true,
+      message: response.data.message,
+      data: null,
+    };
+    return res;
+  }
+  const res = {
+    error: false,
+    message: 'success login',
+    data: response.data.data,
+  };
+  return res;
 }
